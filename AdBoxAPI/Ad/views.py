7d4ffb8f5a32b554c3ef5394f17155f0b37682a7
SalimAliev <calim250899@mail.ru>
2023-05-29 00:04:39 +0300

    install django debab tool bar

--- AdBoxAPI/Ad/views.py
+++ AdBoxAPI/Ad/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 
 def Ad_View(request):
-    return JsonResponse({'response': 'Value1'})
+    # return JsonResponse({'response': 'Value1'})
+    return HttpResponse('<h1>что-то</h1>')
+
--- AdBoxAPI/AdBoxAPI/settings.py
+++ AdBoxAPI/AdBoxAPI/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Ad'
+    "debug_toolbar",
+    'Ad',
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'AdBoxAPI.urls'
@@ -122,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
